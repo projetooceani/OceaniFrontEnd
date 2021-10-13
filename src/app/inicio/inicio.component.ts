16f748bf34aa2fb9f9267a0a873dbc6046e7481c
Fernando-Pozo <fernandopozomqs@gmail.com>
2021-10-12 21:55:19 -0300

    postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -28,6 +28,9 @@ export class InicioComponent implements OnInit {
   nome: string = environment.nome;
   foto: string = environment.foto;
 
+  key = 'data'
+  reverse = true
+
   constructor(
     private router: Router,
     private temaService: TemaService,
