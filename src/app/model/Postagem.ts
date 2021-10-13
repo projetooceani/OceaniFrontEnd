a8e6a713f332ee782941945417613702060a87ee
Fernando-Pozo <fernandopozomqs@gmail.com>
2021-10-13 13:50:34 -0300

    ajustes postagem

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -5,10 +5,10 @@ export class Postagem{
     public id: number;
     public titulo: string;
     public texto: string;
-    public curtida: number;
     public imagem: string;
     public hashtag: string;
     public data: Date;
     public usuario: Usuario;
     public tema: Tema;
+    public curtida: number;
 }
